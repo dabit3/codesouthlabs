26d858490b8b3deb7455ba2c3b42dba674648e8c
dabit3 <dabit3@gmail.com>
2014-11-22 14:54:41 -0600

    added wordpress php files

---
+++ courses/intro-to-wordpress.php
@@ -0,0 +1,107 @@
+<?php include '../includes/coursesheader.php' ?>
+
+<div class="classcontentcontainer">
+	<div class="classcontentwrapper">
+		<div class="classcontentinfo">
+			<h1 class="classcontenth1">INTRO TO PYTHON <br />PROGRAMMING</h1>
+			<h2 class="classcontabout">About this class</h2>
+			<p class="classp">
+				Is web development something you may be considering as a career? Would you like to learn to create your own web sites but don't know where to start? Does the world of programming fascinate you? Are you curious about "what it takes" to be a web developer.
+			</p>
+			<p class="classp">Join a seasoned instructor in this short course designed to introduce you to the world of web development. You'll be introduced to three of the primary languages used for web development: HTML, CSS, and JavaScript.</p>
+			<div class="signupforthisclassinfo">
+				
+				<div class="signupforthisclassheader">
+					<div class="signupforthisclassicon">
+						<i class="fa fa-calendar"></i>
+					</div>
+					<div class="signupforthisclasscopy">
+						<p>January 2015</p>
+						<p>Date &amp; time to be announced.</p>
+					</div>
+					<div class="clearfix"></div>
+
+				</div>
+				<h3 class="signupforthisclassh3">Regular Ticket &nbsp;&nbsp; <span>$85</span></h3>
+				<div class="signupforthisclass">
+				<div class="singupforthisclassbuttonholder">
+					<a data-remodal-target="attendintrotopythonmodal"><p class="signupforthisclassbutton">ATTEND</p></a>
+				</div>
+
+				<div class="signupforthisclassheader">
+					<div class="signupforthisclassicon">
+						<i class="fa fa-map-marker"></i>
+					</div>
+					<div class="signupforthisclasscopy">
+						<p>736 S PRESIDENT ST</p>
+						<p>JACKSON, MS, 39201</p>
+						<p>UNITED STATES</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+
+			</div>
+			
+			</div>
+		</div>
+		<div class="classcontentsidebar">
+			<div class="classcontentsideimage">
+				<img src="../img/courses/introtopython.png" width="250" alt="">
+			</div>
+			<p class="sidebardates">Dates for this class</p>
+			<p class="sidebardate"><i class="fa fa-university"></i>January 2015</p>
+			<p class="sidebardate"><i class="fa fa-university"></i>February 2015</p>
+			<p class="sidebardate"><i class="fa fa-university"></i>March 2015</p>
+			
+			<div class="followclassbuttondiv">
+				<a data-remodal-target="introtopythonmodal"><p class="sidebarfollowbutton"><i class="fa fa-heart"></i>Follow</p></a>
+			</div>
+		</div>
+
+		<div class="classcontentsocial">
+			<div class="classcontentsocialcontainer">
+				<div class="classcontentsocialcontact">
+					<p>Have a question? <a href="../contact">Let us know</a>.</p>
+				</div>
+				<div class="classcontentsocialicons">
+					<a target="_blank" href="https://twitter.com/Codesouthlabs"><i class="fa fa-twitter-square"></i></a>
+					<a href="https://github.com/orgs/CodeSouthLabs" target="_blank"><i class="fa fa-github-square"></i></a>
+					<a href="mailto:info@codesouthlabs.com" target="_blank"><i class="fa fa-envelope-square"></i></a>
+
+				</div>
+			</div>
+		</div>
+	
+
+
+		<div class="clearfix"></div>
+	</div>
+</div>
+
+<!-- ************** Modal to attend Wordpress Course ***************** -->
+
+<div class="remodal" data-remodal-id="attendintrotopythonmodal">
+		<h1>Intro To Python Programming</h1>
+	     <p style="">The date for this course has not yet been announced. Please follow this course to be notified of the next class date. Thanks!</p>
+	     <a class="remodal-confirm" href="#" >OK</a>
+</div>
+
+<div class="remodal" data-remodal-id="introtopythonmodal">
+    <div class="beforesignedup">
+    	<h1>Stay updated</h1>
+	     <p style="">Enter Your Email Address To Start Following.</p>
+	        <div class="remodalemailform">
+	        	<form>
+		        	<input type="text" class="class-email" id="class-email" placeholder="email"><br />
+		        	<input id="class-introtopython-follow" class="classfollow" type="button" value="Follow Class" />
+		        </form>
+	       	</div>
+    </div>
+		<div class="aftersignedup" style="display: none;;">
+				<h1>You will be updated on future dates of this course!</h1>
+				<a class="remodal-confirm" href="#" onclick="window.location.replace('../index')">OK</a>
+		</div>
+
+</div>
+
+<?php include '../includes/coursesfooter.php' ?>
