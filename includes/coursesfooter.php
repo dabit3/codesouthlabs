6f0fb3c68f1c85711bd1142d8c781385d5ca68dc
dabit3 <dabit3@gmail.com>
2014-11-22 13:54:54 -0600

    added python info

---
+++ includes/coursesfooter.php
@@ -0,0 +1,42 @@
+
+<footer>
+	<div class="footer-container">
+		<section class="footer-inner">
+			
+			<section class="footer-row">
+				<p class="footer-row-heading">PARTNERS</p>
+				<ul class="footerul">
+					<a target="_blank" href="http://www.workinthehive.com"><li>#HIVE</li></a>
+					<a target="_blank" href="https://twitter.com/jacksondevs"><li>@jacksondevs</li></a>
+					<a target="_blank" href="http://www.naderdabit.me"><li>naderdabit.me</li></a>
+				</ul>
+			</section>
+
+			<section class="footer-row">
+				<p class="footer-row-heading">CONTACT</p>
+				<ul class="footerul">
+					<a href="mailto:info@codesouthlabs.com"><li>info@codesouthlabs.com</li></a>
+					<a href="mailto:teach@codesouthlabs.com"><li>teach@codesouthlabs.com</li></a>
+				</ul>
+			</section>
+
+			<section class="footer-row">
+				<p class="footer-row-heading">SOCIAL</p>
+				<ul class="footerul">
+					<a target="_blank" href="https://twitter.com/Codesouthlabs"><li>twitter</li></a>
+				</ul>
+			</section>
+
+			<div class="clearfix"></div>
+		</section>
+	</div>
+</footer>
+
+<script src="../lib/jquery-2.1.0.min/index.js"></script>
+<script src="../lib/scrollRevealjs/dist/scrollReveal.min.js"></script>
+<script src="../lib/greensock/src/minified/TweenMax.min.js"></script>
+<script src="../js/main.js"></script>
+<script src="../lib/fastclick/lib/fastclick.js"></script>
+<script src="../lib/remodal/dist/jquery.remodal.min.js"></script>
+</body>
+</html>
---
+++ includes/coursesheader.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta charset="UTF-8">
+	<title>Code South Labs</title>
+	<link rel="stylesheet" href="../lib/normalize-css/normalize.css">
+
+	<script src="//use.typekit.net/vtj6dqq.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+	<script>try{Typekit.load();}catch(e){}</script>
+	<link rel="stylesheet" href="../lib/remodal/dist/jquery.remodal.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<link rel="stylesheet" href="../lib/font-awesome/css/font-awesome.min.css">
+
+
+</head>
+
+<header class="wide-header">
+	<section class="header-container">
+		<div class="header-content">
+			<a href="../index"><img src="../img/logowhite.png" width="250" alt="" class="logo"></a>
+			<div style="" class="teach-button"><a href="../teach"><p>TEACH</p></a></div>
+			<div class="nav-container">
+				<ul class="main-nav">
+					<li><a href="all">COURSES</a></li>
+					<li><a href="../contact">CONTACT</a></li>
+					<li><a href="../about">ABOUT</a></li>		
+					<li><a href="../index">HOME</a></li>			
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</section>
+</header>
+
+<section class="header-scroll">
+	<section class="header-scroll-container">
+		<div class="header-scroll-content">
+			<a href="../index"><img src="../img/logowhite.png" width="250" alt="" class="logo"></a>
+		</div>
+		<div class="hamburger">
+			<img src="../img/hamburger.png" alt="">
+		</div>
+		<div class="clearfix"></div>
+	</section>
+</section>
+
+<!-- Scroller menu -->
+
+<section class="overlay">
+	<div class="overlay-content">
+		<div class="overlay-menu">
+			<div class="close"><p>x</p></div>
+			<ul class="overlay-ul">
+				<a href="../index"><li>HOME</li></a>
+				<a href="all"><li>COURSES</li></a>
+				<a href="../contact"><li>CONTACT</li></a>
+				<a href="../about"><li>ABOUT</li></a>
+				
+				<a href="../teach.php"><li>TEACH</li></a>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<!-- Mobile Menu -->
+
+
+<div class="mymobile">
+	<section class="mobile-bar">
+		<section class="mobile-bar-container">
+			<div class="mobile-bar-content">
+				<a href="../index"><img src="../img/logowhite.png" width="200" alt="" class="logo"></a>
+			</div>
+			<div class="clearfix"></div>
+		</section>
+	</section>
+</div>
+
+<div class="mobile-hamburger">
+		<img src="../img/hamburger.png" alt="">
+</div>
+
+<div class="mobile-menu">
+	<div class="mobile-menu-container">
+		<div class="mobileul">
+			<ul>
+				<a href="../index"><li>HOME</li></a>
+				<a href="all"><li>COURSES</li></a>
+				<a href="../contact"><li>CONTACT</li></a>
+				<a href="../about"><li>ABOUT</li></a>
+				
+				<a href="../teach.php"><li>TEACH</li></a>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<body>
